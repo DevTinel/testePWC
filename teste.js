bd24ee549361a5948be9a816f14988b1e5855ca8
DevTinel <davidaugustolol36@gmail.com>
2023-07-04 22:21:59 -0300

    remocao caracter unico

--- teste.js
+++ teste.js
@@ -5,6 +5,23 @@
  * b. Output:"amazing is OpenAI World!Hello."
  */
 
-const text = "Hello ,World! OpenAI is amazing.";
+// const textEx1 = "Hello ,World! OpenAI is amazing.";
 
-console.log(text.split(" ").reverse().join(" "));
+// console.log(text.split(" ").reverse().join(" "));
+
+/**
+ * 2. Remova todos os caracteres duplicados da string abaixo
+ * a. Input: "Hello, World!"
+ * b. Output: "Helo,Wrd!"
+ */
+
+const textEx2 = "Hello, World";
+
+let caracteres = "";
+
+for (let i = 0; i < textEx2.length; i++) {
+    if (caracteres.indexOf(textEx2[i]) === -1) {
+        caracteres += textEx2[i];
+    }
+}
+console.log(caracteres);
