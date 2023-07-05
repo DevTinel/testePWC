3909d425b2ce58c12d5dcf6366dcb8d7f8ad800e
DevTinel <davidaugustolol36@gmail.com>
2023-07-04 23:04:40 -0300

    capitalizar a primeira letra de cada frase

--- teste.js
+++ teste.js
@@ -15,13 +15,45 @@
  * b. Output: "Helo,Wrd!"
  */
 
-const textEx2 = "Hello, World";
+// const textEx2 = "Hello, World";
 
-let caracteres = "";
+// let caracteres = "";
 
-for (let i = 0; i < textEx2.length; i++) {
-    if (caracteres.indexOf(textEx2[i]) === -1) {
-        caracteres += textEx2[i];
-    }
+// for (let i = 0; i < textEx2.length; i++) {
+//     if (caracteres.indexOf(textEx2[i]) === -1) {
+//         caracteres += textEx2[i];
+//     }
+// }
+// console.log(caracteres);
+
+/**Encontre  a substring palindroma mais longa na string abaixo
+ * String Palindromo definição : Uma palavra é dita palindroma quando
+ * possui o mesmo significado se lida da esquerda para a direita ou
+ * da direita para esqurda
+ * a. Input: "babad"
+ * b. Output: "bab"(Observação : pode haver várias saídas válidas ,você só precisa retomar
+ * uma delas.))
+ */
+
+let textEx3 = "babad";
+
+/**
+ * Coloque em maiuscula a primeira letra de cada frase na string
+ * a. Input: "Hello. how are you? i'm fine,thank you."
+ * b. Output: "Hello.How are you?"I'm fine,thank you.""
+ */
+
+function capitalizarPrimeiraLetra() {
+    let textEx4 = "hello. how are you? i'm fine,thank you.".split(".");
+
+    let fraseCapitalizada = textEx4.map((frase) => {
+        frase = frase.trim();
+
+        if (frase.length == 0) {
+            return frase;
+        }
+        return frase.charAt(0).toLocaleUpperCase() + frase.slice(1);
+    });
+    return fraseCapitalizada.join(".");
 }
-console.log(caracteres);
+console.log(capitalizarPrimeiraLetra());
