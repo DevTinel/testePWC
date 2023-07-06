6312ac33b90711ccb07bea1e0ae1bd70d050ae5b
DevTinel <davidaugustolol36@gmail.com>
2023-07-05 23:27:17 -0300

    capitalizarPrimeiraLetra

--- tarefa.js
+++ tarefa.js
@@ -17,3 +17,20 @@ function removerCaracteresDuplicados(string) {
     }
     return caracteres;
 }
+
+function capitalizarPrimeiraLetra(string) {
+    let frases = string.split(".");
+
+    let fraseCapitalizada = frases.map((frase) => {
+        frase = frase.trim();
+
+        if (frase.length == 0) {
+            return frase;
+        }
+        return frase.charAt(0).toLocaleUpperCase() + frase.slice(1);
+    });
+    return fraseCapitalizada.join(".");
+}
+console.log(
+    capitalizarPrimeiraLetra("hello. how are you? i'm fine,thank you.")
+);
